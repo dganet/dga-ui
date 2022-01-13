3a4c120897e673327c8b66e9d50ade2e23d8b7fe
Guilherme Brito <guilhermebritto.prof@gmail.com>
2022-01-13 10:04:41 -0300

    fix: removido a importação  do cardbody

--- src/components/Card/CardComponent.tsx
+++ src/components/Card/CardComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { useTheme } from '@hooks/useTheme'
-import { Container, CardBody } from './CardComponent.styles'
+import { Container } from './CardComponent.styles'
 import { CardProps } from './CardComponent.types'
 
 const CardComponent = ({ children, cardType }: CardProps) => {
